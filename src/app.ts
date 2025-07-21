715b7f5656acc312e21b3ffcd1d02a80a20fff57
Branimir Yordanov <branimiryordanov75@gmail.com>
2025-07-21 09:40:02 +0300

    setup

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import path from 'path';
+import expressLayouts from 'express-ejs-layouts'
+
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(expressLayouts);
+
+app.set('layout', 'layouts/default'); 
+
+app.get('/', (req,res) => {
+    res.render('pages/index', { title: 'Home' });   
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);    
+})
