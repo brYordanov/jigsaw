1d4d58183ca3fee9078603efe3db720c6afd224b
BranimirYo <branimiryordanov75@gmail.com>
2025-08-21 22:49:34 +0300

    file structure

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,22 @@
-import express from 'express';
-import path from 'path';
-import expressLayouts from 'express-ejs-layouts';
+import express from 'express'
+import path from 'path'
+import expressLayouts from 'express-ejs-layouts'
+import { viewRouter } from './views/intex'
+import { apiRouter } from './api'
 
 const app = express()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(expressLayouts);
+app.use(express.static(path.join(__dirname, '../public')))
+app.use(expressLayouts)
 
-app.set('layout', 'layouts/default'); 
-
-app.get('/', (req,res) => {
-    res.render('pages/index', { title: 'Home' });   
-})
+app.set('layout', 'layouts/default')
+app.use('/', viewRouter)
+app.use('/api', apiRouter)
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);    
+  console.log(`Server is running on http://localhost:${PORT}`)
 })
