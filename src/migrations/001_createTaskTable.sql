9e1d107139d3a5521b4b9a1ffda6c0645d48a263
BranimirYo <branimiryordanov75@gmail.com>
2025-08-25 23:42:13 +0300

    WIP

--- src/migrations/001_createTaskTable.sql
+++ src/migrations/001_createTaskTable.sql
@@ -1,12 +1,12 @@
 CREATE TABLE tasks (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    description TEXT,
-    schedule_type TEXT NOT NULL CHECK (schedule_type IN ('fixed', 'deadman')),
-    is_single_time_only BOOLEAN NOT NULL DEFAULT TRUE,
-    type TEXT NOT NULL CHECK (type IN ('monthly', 'weekly', 'daily', 'hourly')),
-    days INTEGER[],
-    jump INTEGER[],
-    minutes INTEGER[],
-    created_at TIMESTAMP DEFAULT NOW()
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT,
+  schedule_type TEXT NOT NULL CHECK (schedule_type IN ('fixed', 'deadman')),
+  is_single_time_only BOOLEAN NOT NULL DEFAULT TRUE,
+  type TEXT NOT NULL CHECK (type IN ('monthly', 'weekly', 'daily', 'hourly')),
+  days INTEGER[],
+  minutes INTEGER[],
+  hours INTEGER[],
+  jump INTEGER[] created_at TIMESTAMP DEFAULT NOW()
 );
