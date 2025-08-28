5689e1d37a4ff870c4f1851af0d8652e048cdd8e
Branimir Yordanov <branimiryordanov75@gmail.com>
2025-08-28 14:44:43 +0300

    starting scripts and DB connection

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import expressLayouts from 'express-ejs-layouts'
 import { viewRouter } from './views/intex'
 import { apiRouter } from './api'
+import { pool, shutdownDb } from './db/db'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -17,6 +18,25 @@ app.set('layout', 'layouts/default')
 app.use('/', viewRouter)
 app.use('/api', apiRouter)
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
+async function start() {
+    await pool.query('SELECT 1')
+    app.listen(PORT, () => {
+        console.log(`✅ Server is running on http://localhost:${PORT}`)
+    })
+}
+
+const downSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM']
+downSignals.forEach(sig => {
+    process.on(sig, async () => {
+        try {
+            await shutdownDb()
+        } finally {
+            process.exit(0)
+        }
+    })
+})
+
+start().catch(err => {
+    console.error('❌ Failed to start server:', err)
+    process.exit(1)
 })
