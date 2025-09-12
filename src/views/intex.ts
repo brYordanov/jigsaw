ea3b3bdfea935f60b1724e2c1c54c0eeca049dfa
Branimir Yordanov <branimiryordanov75@gmail.com>
2025-09-12 17:38:29 +0300

    filters and pagination

--- src/views/intex.ts
+++ src/views/intex.ts
@@ -3,4 +3,7 @@ import { ViewTaskRouter } from './routes/task.router'
 
 export const viewRouter = Router()
 
+viewRouter.get('/', (req, res) => {
+    res.render('pages/index')
+})
 viewRouter.use('/task', ViewTaskRouter)
