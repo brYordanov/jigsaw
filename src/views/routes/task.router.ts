11f910c29fff848456b2c36681cff9b961dd5cf7
Branimir Yordanov <branimiryordanov75@gmail.com>
2025-09-15 15:11:17 +0300

    task edit page

--- src/views/routes/task.router.ts
+++ src/views/routes/task.router.ts
@@ -1,51 +1,25 @@
 import { Router } from 'express'
-import { createTaskSchema, listTasksQuerySchema } from '../../modules/tasks/task.dtos'
+import {
+    createTaskSchema,
+    listTasksQuerySchema,
+    updateTaskSchema,
+} from '../../modules/tasks/task.dtos'
 import { TaskService } from '../../modules/tasks/task.service'
 import { HttpStatus } from '../../helpers/statusCodes'
 import { coerseFormValuesMD } from '../../middlewares/coerseFormValues'
 import { groupZodIssues } from '../../helpers/groupZodIssues'
 import { ZodError } from 'zod'
 import { normalizeFormValues } from '../../middlewares/normalizeFormValues'
+import { getPaginationData } from '../../helpers/getPaginationData'
+import { error } from 'console'
 
 export const ViewTaskRouter = Router()
 const service = new TaskService()
 
-function buildPager({
-    limit,
-    offset,
-    total,
-    filters,
-}: {
-    limit: number
-    offset: number
-    total: number
-    filters: Record<string, any>
-}) {
-    const toQueryString = (obj: Record<string, any>) =>
-        '?' +
-        Object.entries(obj)
-            .filter(([_, v]) => v !== undefined && v !== null && v !== '')
-            .map(([k, v]) => encodeURIComponent(k) + '=' + encodeURIComponent(v))
-            .join('&')
-
-    const prevOffset = Math.max(0, offset - limit)
-    const nextOffset = offset + limit < total ? offset + limit : null
-
-    return {
-        hasPrev: offset > 0,
-        hasNext: nextOffset !== null,
-        prevHref: offset > 0 ? toQueryString({ ...filters, limit, offset: prevOffset }) : null,
-        nextHref:
-            nextOffset !== null ? toQueryString({ ...filters, limit, offset: nextOffset }) : null,
-        page: Math.floor(offset / limit) + 1,
-        totalPages: Math.ceil(total / limit),
-    }
-}
-
 ViewTaskRouter.get('/', async (req, res) => {
     const params = listTasksQuerySchema.parse(req.query)
     const { items: tasks, total, limit, offset } = await service.paginate(params)
-    const paginateData = buildPager({ limit, offset, total, filters: params })
+    const paginateData = getPaginationData({ limit, offset, total, filters: params })
 
     if (req.get('HX-Request') === 'true') {
         return res.render('partials/task-list-section', {
@@ -78,6 +52,34 @@ ViewTaskRouter.post('/create', coerseFormValuesMD, async (req, res) => {
             const { firstPerField, grouped } = groupZodIssues(err.issues)
 
             return res.status(HttpStatus.UNPROCESSABLE_ENTITY).render('pages/task-create', {
+                values: normalizeFormValues(req.body),
+                errors: firstPerField,
+            })
+        }
+
+        console.error(err)
+        return res.status(HttpStatus.UNEXPECTED_SERVER_ERROR).render('errors/500')
+    }
+})
+
+ViewTaskRouter.get('/edit/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    const task = await service.getByIdOrFail(id)
+    res.render('pages/task-edit', { values: task, errors: {} })
+})
+
+ViewTaskRouter.post('/edit/:id', coerseFormValuesMD, async (req, res) => {
+    try {
+        const id = Number(req.params.id)
+        const dto = updateTaskSchema.parse(req.body)
+        const task = await service.updateTask(id, dto)
+
+        return res.redirect(`/task`)
+    } catch (err) {
+        if (err instanceof ZodError) {
+            const { firstPerField, grouped } = groupZodIssues(err.issues)
+
+            return res.status(HttpStatus.UNPROCESSABLE_ENTITY).render('pages/task-edit', {
                 values: normalizeFormValues(req.body),
                 errors: firstPerField,
             })
