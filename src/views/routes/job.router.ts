317a68aabb63b8b8748bfc5c1b65fbb4971d3993
BranimirYo <branimiryordanov75@gmail.com>
2025-09-23 20:02:09 +0300

    job types WIP

--- src/views/routes/job.router.ts
+++ src/views/routes/job.router.ts
@@ -13,6 +13,25 @@ ViewJobRouter.get('/create', (req, res) => {
 
 ViewJobRouter.post('/create', (req, res) => {
     const body = req.body
-    validateJobConfig(validatorsByType[body.job_type], body.config)
+    // validateJobConfig(validatorsByType[body.job_type], body.config)
     console.log(body)
+})
+
+ViewJobRouter.get('/config-partial', (req, res) => {
+    try {
+        const jobType = String(req.query.job_type)
+        const known = ['http', 'email', 'shell', 'sql', 'healthcheck']
+        if (!known.includes(jobType)) {
+            throw new Error('Unknown job type')
+        }
+        const partial = jobType
+
+        return res.render(`partials/job-config-fields/${partial}.ejs`, {
+            values: {},
+            errors: {},
+            layout: false,
+        })
+    } catch (err) {
+        res.status(400).json({ error: (err as Error).message })
+    }
 })
